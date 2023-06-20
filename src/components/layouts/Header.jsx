509c1d72cb59c10452e24f0d53abbed6cb38dfeb
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-06-20 22:51:54 +0700

    update

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <div className='main-content'>
-      <header className='flex items-center justify-center pb-3 h-[110px] mb-5 text-white header gap-x-5 z-10 bg-[#191A2E]'>
+      <header className='flex items-center justify-center pb-3 h-[110px] mb-5 text-white header gap-x-5 z-[999] bg-[#191A2E]'>
         <NavLink
           to='/'
           className={({ isActive }) => (isActive ? 'text-primary' : '')}
